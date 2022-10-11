22ad61403c784e8eecdb4396733291a7bee8c308
madridandres <madridandres.107.am@outlook.com>
2022-10-11 05:44:42 -0500

    Se creó: SHOW PRODUCTS. Se agregó: función dark-mode, animación al cargar la página y menú amburguesa, se corrigieron errores de desbordamiento en la imagen principal y se unieron los botones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,66 @@
 // prices products 
 const items = [
-    {
-      id: 1,
-      name: 'Hoodies',
-      price: 14.00,
-      image: 'assets/images/featured1.png',
-      category: 'hoodies',
-      quantity: 10
-    },
-    {
-      id: 2,
-      name: 'Shirts',
-      price: 24.00,
-      image: 'assets/images/featured2.png',
-      category: 'shirts',
-      quantity: 15
-    },
-    {
-      id: 3,
-      name: 'Sweatshirts',
-      price: 24.00,
-      image: 'assets/images/featured3.png',
-      category: 'Sweatshirts',
-      quantity: 20
-    }
-  ]
+  {
+    id: 1,
+    name: 'Hoodies',
+    price: 14.00,
+    image: 'assets/images/featured1.png',
+    category: 'hoodies',
+    quantity: 10
+  },
+  {
+    id: 2,
+    name: 'Shirts',
+    price: 24.00,
+    image: 'assets/images/featured2.png',
+    category: 'shirts',
+    quantity: 15
+  },
+  {
+    id: 3,
+    name: 'Sweatshirts',
+    price: 24.00,
+    image: 'assets/images/featured3.png',
+    category: 'Sweatshirts',
+    quantity: 20
+  }
+]
 
+// Evento DOMContentLoaded
+
+
+const themeIcon = document.getElementById( "theme-btn" )
+
+themeIcon.addEventListener("click", () =>{
+  document.body.classList.toggle("dark-theme")
+  if(themeIcon.classList.contains("bx-moon")){
+    themeIcon.classList.replace("bx-moon", "bx-sun")
+  }else{
+    themeIcon.classList.replace("bx-sun", "bx-moon")
+  }
+})
+
+//Ubicación del elemento loader dentro del DOM
+
+const loadComponent = () => {
+  const loader = document.getElementById("loader")
+
+  setTimeout(() => {
+    loader.classList.add("hide")
+  }, 3000)
+    
+}
+// Todas las funciones que se necesiten al cargar la pagina
+// Se pueden agregar en este DOMContentLoaded
+document.addEventListener( "DOMContentLoaded", () =>{
+  loadComponent()
+})
+
+
+//MENÚ AMBURGUESA
+
+const menuBtn = document.getElementById("menu-btn")
+const menu = document.querySelector(".nav-menu")
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("visibility")
+})
